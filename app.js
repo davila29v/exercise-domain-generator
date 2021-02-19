aea92ebc5410db15badc909b22b964d24163d000
davila29v <davila29v@hotmail.com>
2021-02-19 01:38:00 +0000

    ejercicio listo

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.net', '.us', '.io']
+
+
+pronoun.forEach(pronumElement => {
+    adj.forEach(adjElement => {
+        noun.forEach(nounElement => {
+            extension.forEach(elementExtension => {
+                console.log(pronumElement + adjElement + elementExtension);
+            });
+        });
+    });
+});
